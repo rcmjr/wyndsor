864708b99bc52ca60466a1aea001416625f2de43
RM <robmrphyjr@gmail.com>
2018-02-19 22:09:08 -0500

    Manually merging dapper into master branch.

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,29 +1,152 @@
-'use strict';
+var $                 = require('gulp-load-plugins')({
+                        				lazy: true,
+                        				camelize: true}),
+    argv              = require('yargs').argv,
+    browser           = require('browser-sync'),
+    gulp              = require('gulp'),
+    sass              = require('gulp-sass'),
+    sequence          = require('run-sequence'),
+    inject            = require('gulp-inject'),
+    cleanCSS          = require('gulp-clean-css'),
+    gulpcached        = require('gulp-cached'),
+    gulpif            = require('gulp-if'),
+    concat            = require('gulp-concat'),
+    imported          = require('gulp-sass-partials-imported'),
 
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var sourcemaps = require('gulp-sourcemaps');
-var changedInPlace = require('gulp-changed-in-place');
-var gulpIf = require('gulp-if');
+    // Check for --dev or --prod flag
+    isDevelopment     = !!(argv.dev),
+    isProduction      = !!(argv.prod),
 
-var sassOptions = {
-  errLogToConsole: true,
-  outputStyle: 'nested'
-};
-var SRC = ['**/*.scss',
-            '!node_modules/**/*.scss',
-            '!_Dev/*.scss'];
-var DEST = './docs/css/';
+    // Gulp-Sass Options
+    sassOptions       = {errLogToConsole: true, outputStyle: 'nested'};
 
-gulp.task('workflow', function () {
-  gulp.src(SRC)
-        .pipe(changedInPlace())
-        .pipe(sourcemaps.init())
-        .pipe(sass(sassOptions).on('error', sass.logError))
-        .pipe(sourcemaps.write('./'))
-        .pipe(gulp.dest(DEST))
-      });
+    // Source, Destination and Port Vars
+    var ROOT                = 'docs/',
+        SRC_ROOT            = 'Style/',
+        SRC_ALL             = SRC_ROOT + '**/*.scss',
+        DEST_STYLE          = ROOT + 'css/',
+        DEST_STYLES_DEV     = DEST_STYLE + 'dev/',
+        HTML_ROOT           = ROOT,
+        DEST_HTML_ALL       = HTML_ROOT + '**/*.html',
+        PORT                = 8000;
 
-gulp.task('default', function () {
-  gulp.watch('./**/*.scss', ['workflow']);
-});
+    // Prime Vars
+    var STYLE               = 'style.scss',
+        STYLE_CSS           = 'style.css',
+        STYLE_BASE          = 'Admin/_base.scss',
+        STYLE_DEPENDANCIES  = ['Admin/_reset.scss',
+                                'Tools/C-Fonts/_fonts.scss',
+                                'Tools/_toolbox.scss',
+                                'Tools/B-Mixins/Animations-css/_animations.scss',
+                                'Tools/B-Mixins/Hover-css/_keyframes.scss',
+                                'Tools/B-Mixins/FontAwesome/_awesomeness.scss'],
+        STYLE_MAPS          = 'Admin/_maps.scss',
+        STYLE_EPIC          = 'Admin/Core/_the-epic.scss';
+
+    // Build Vars
+    server_build_load       = (isDevelopment === true) ? ['build'] : null,
+    scss_build_task         = $.if(isDevelopment, ['build'], ['inject_style-PROD']),
+    style_build             = $.if(isDevelopment, [STYLE], [STYLE_MAPS]),
+    style_build_task        = $.if(isDevelopment, ['inject_style-DEV'], ['inject_style-PROD']),
+    style_head_inject       = $.if(isDevelopment, '**/*.css', STYLE_CSS);
+
+    let src_partials = 'docs/gulp-cache/';
+
+    // Messages
+    if (isDevelopment) {
+      console.log('Starting wyndsor in dev mode.')
+    } else {
+      console.log('Starting wyndsor in prod mode')
+    }
+
+  ///////////////////////////////////////////////////
+  // Injects dependencies into individual style scss files only when in dev mode
+  gulp.task('build', function () {
+    gulp.src(SRC_ALL)
+
+    // Uses only partials that have changed
+    .pipe(gulpcached('sassfiles'))
+
+    // Injects dependencies into dev files
+    .pipe($.inject(gulp.src(STYLE_BASE, {read: false}), {
+      relative: true,
+      starttag: '// inject: dependenciesbase //',
+      endtag: '// endinject: dependenciesbase //',
+      transform: function (filepath) {
+          return '@import "' + filepath + '";';
+      }}))
+    .pipe($.inject(gulp.src(STYLE_EPIC, {read: false}), {
+      relative: true,
+      starttag: '// inject: dependenciesepic //',
+      endtag: '// endinject: dependenciesepic //',
+      transform: function (filepath) {
+          return '@import "' + filepath + '";';
+      }}))
+
+      // Sends error if something goes wrong
+      .pipe(sass(sassOptions).on('error', sass.logError))
+
+      // Declares css destination as docs/css/Style
+      .pipe(gulp.dest(DEST_STYLES_DEV))
+      .pipe(browser.reload({stream: true}));
+  });
+
+  // Imports for Prime DEV
+  gulp.task('inject_style-DEV', function () {
+    var sources = gulp.src(STYLE_DEPENDANCIES, {read: false});
+    gulp.src(STYLE)
+    .pipe(inject(sources, {relative: true,
+      starttag: '// inject: styledependencies //',
+      endtag: '// endinject: styledependencies //',
+      transform: function (filepath) {
+          return '@import "' + filepath + '";';}}))
+      // Sends error if something goes wrong
+      .pipe(sass(sassOptions).on('error', sass.logError))
+      // Declares css destination
+      .pipe(gulp.dest(DEST_STYLE))
+      .pipe(browser.reload({stream: true}));
+  });
+
+  // Imports for PROD
+  gulp.task('inject_style-PROD', function () {
+
+    // Adds base, all styles, the maps and the epic to style.scss
+    STYLE_DEPENDANCIES.unshift(STYLE_BASE);
+    STYLE_DEPENDANCIES.push(SRC_ALL, STYLE_MAPS, STYLE_EPIC);
+    var sources = gulp.src(STYLE_DEPENDANCIES, {read: false});
+    gulp.src(STYLE)
+    // Starts injection
+    .pipe(inject(sources, {relative: true,
+      starttag: '// inject: styledependencies //',
+      endtag: '// endinject: styledependencies //',
+      transform: function (filepath) {
+          return '@import "' + filepath + '";';}}))
+      // Sends error if something goes wrong
+      .pipe(sass(sassOptions).on('error', sass.logError))
+      // Declares css destination
+      .pipe(gulp.dest(DEST_STYLE))
+      .pipe(browser.reload({stream: true}));
+  });
+
+  // Injects css files into html header // WORKING
+  gulp.task('inject_header', function () {
+    var target  = gulp.src(DEST_HTML_ALL);
+    var sources = gulp.src([DEST_STYLE + style_head_inject], {read: false});
+    return target.pipe(inject(sources, {relative: true}))
+      .pipe(gulp.dest(HTML_ROOT))
+  });
+
+  ////////////////////////////////////////
+  // Start a server with LiveReload to preview the site in  /// WORKING
+  gulp.task('server', server_build_load, function() {
+    browser.init({
+      server: 'docs', port: PORT
+    });
+  });
+
+  // Watches for changes
+  gulp.task('default', ['server'], function() {
+    gulp.watch([SRC_ALL], scss_build_task); // Watches individual scss
+    gulp.watch(style_build, style_build_task); // Watches style SCSS
+    gulp.watch([DEST_HTML_ALL], ['inject_header', browser.reload]); // Reloads if html files updates
+  });
